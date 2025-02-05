3bb6dfe47cd4c161abd6fa144a7e461b6d110008
DaniDeveloper11 <151704893+DaniDeveloper11@users.noreply.github.com>
2025-02-05 06:23:33 -0600

    fix: client Managament

--- src/app/features/clients/pages/list-clients/list-clients.component.ts
+++ src/app/features/clients/pages/list-clients/list-clients.component.ts
@@ -8,6 +8,11 @@ import { ClientsService, Client } from '../../../../core/services/clients.servic
 })
 export class ListClientsComponent implements OnInit {
   clients: Client[] = [];
+  filteredClients: Client[] = []
+  searchQuery: string = '';
+  statusFilter: string = '';
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
 
   constructor(private clientsService: ClientsService) {}
 
@@ -18,12 +23,29 @@ export class ListClientsComponent implements OnInit {
   fetchClients(): void {
     this.clientsService.getClients().subscribe((clients: Client[]) => { // 👈 Agregar el tipo explícitamente
       this.clients = clients;
+      this.applyFilters();
     });
+  }
+
+  applyFilters(): void {
+    this.filteredClients = this.clients.filter(client => {
+      return (
+        (this.searchQuery === '' || client.name.toLowerCase().includes(this.searchQuery.toLowerCase())) &&
+        (this.statusFilter === '' || client.status === this.statusFilter)
+      );
+    });
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page;
   }
 
   deleteClient(id: number): void {
     this.clientsService.deleteClient(id).subscribe(() => {
       this.fetchClients();
     });
+  }
+  getTotalPages(): number[] {
+    return Array.from({ length: Math.ceil(this.filteredClients.length / this.itemsPerPage) }, (_, i) => i + 1);
   }
 }
