f62db365e3179fb62058f848aedbb3dc5bca6ca2
DaniDeveloper11 <151704893+DaniDeveloper11@users.noreply.github.com>
2025-02-04 22:41:42 -0600

    add: auth-redirect guard

---
+++ src/app/core/guards/auth-redirect.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthRedirectGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/home']); // Redirige a Home si ya está autenticado
+      return false;
+    }
+    return true;
+  }
+}
--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { AuthRedirectGuard } from '../../core/guards/auth-redirect.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent, canActivate: [AuthRedirectGuard] }
 ];
 
 @NgModule({
